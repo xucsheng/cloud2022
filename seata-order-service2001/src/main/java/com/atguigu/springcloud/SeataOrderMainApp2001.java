2cf5fecda46539246844795297ce6f36520d04ef
xuchunsheng <xuchunsheng123>
2022-07-20 13:31:21 +0800

    springcloudalibaba代码，20220720

--- seata-order-service2001/src/main/java/com/atguigu/springcloud/SeataOrderMainApp2001.java
+++ seata-order-service2001/src/main/java/com/atguigu/springcloud/SeataOrderMainApp2001.java
@@ -2,8 +2,6 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -13,7 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @EnableDiscoveryClient
 @EnableFeignClients
-@SpringBootApplication(scanBasePackages = "com.atguigu.springcloud", exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class SeataOrderMainApp2001 {
     public static void main( String[] args ) {
         SpringApplication.run(SeataOrderMainApp2001.class,args);
